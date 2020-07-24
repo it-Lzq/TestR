a8be79c2165bb8e176d9f75e4cd2d8b28e75eecb
it-lzq <905866484@qq.com>
2020-07-24 12:50:32 +0800

    用户类的创建

---
+++ test01/src/com.lzq.model/User.java
@@ -0,0 +1,12 @@
+package com.lzq.model;
+
+/**
+ * @author 作者:李泽庆
+ * @version 创建时间:2020/7/24 12:45
+ * @email 邮箱:905866484@qq.com
+ * @description 描述：用户
+ */
+public class User {
+    private Long id;
+    private String name;
+}
