c2d8ff5a0bc4c853c600b0aa3ad7d9538d38e173
it-lzq <905866484@qq.com>
2020-07-24 14:03:36 +0800

    用户类的创建，第一次提交

--- test01/src/com.lzq.model/User.java
+++ test01/src/com.lzq.model/User.java
@@ -20,19 +20,4 @@ public class User {
         this.name = name;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
